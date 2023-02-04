15430e523ff02c92544d8e64ef84a4e977634a3f
Yazdan Ranjbar <yazdanra@icloud.com>
2023-02-04 05:32:09 -0600

    Remove phone number from registration form

--- src/apps/account/authentication/serializers/register_serializer.py
+++ src/apps/account/authentication/serializers/register_serializer.py
@@ -6,7 +6,7 @@ from apps.account.user.models import User
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["first_name", "last_name", "email", "phone_number", "password"]
+        fields = ["first_name", "last_name", "email", "password"]
         extra_kwargs = {
             "password": {"write_only": True},
         }
