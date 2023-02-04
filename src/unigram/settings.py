cf0fd21c59fa468ba3e9c60bd150e49080105dc7
Yazdan Ranjbar <yazdanra@icloud.com>
2023-02-04 04:37:50 -0600

    Update REST and CORS config

--- src/unigram/settings.py
+++ src/unigram/settings.py
@@ -171,11 +171,31 @@ LOGGING = {
 
 # Django Rest Framework
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.TokenAuthentication",
+    "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
-    ),
-    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"]
+    ],
+    "DATETIME_FORMAT": "%m/%d/%Y %H:%M",
+    "DATE_INPUT_FORMATS": ["%Y/%m/%d"],
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter",
+    ],
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "500000/day",
+        "user": "100000/day"
+    },
+
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = getenv("CORS_ALLOWED_ORIGINS", "api.24hour.yazdanra.com,35.232.144.44").split(",")
+CORS_ALLOW_ALL_ORIGINS = DEBUG
+CORS_ALLOW_CREDENTIALS = True
+CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS
